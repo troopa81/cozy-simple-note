574cb50ff109235e786a93061a9b3d9634716b55
Julien Cabieces <cabieces.julien@gmail.com>
2016-05-16 17:13:27 +0200

    integration of angular and material design

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,132 +1,163 @@
-function routerHandler() {
 
-    var $panels = $('[role="panel"]');
-    var $menuItems = $('[role="navigation"] li');
+var app = angular.module('SimpleNoteApp', ['ngMaterial', 'ngRoute'] );
 
-    function handleRouteChange() {
+app.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when( '/', {
+			templateUrl : 'listNotes.html', 
+			controller : 'mainController'
+		})
+		.when ( '/edit', {
+			templateUrl : 'editNote.html', 
+			controller : 'editController'
+		}); 
 
-        var hash;
-        // Default screen management.
-        if(window.location.hash.length === 0) {
-            hash = "#crud-create";
-        } else {
-            hash = window.location.hash;
-        }
+	// use the HTML5 History API
+    $locationProvider.html5Mode(true);
+});
 
-        // Reset panels visibility.
-        $panels.hide();
+app.controller('mainController', ['$scope', function($scope) {
+/*
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+*/
+ 
+}]);
 
-        // Select panel, and show it.
-        var $panel = $panels.filter(hash).show();
+app.controller('editController', ['$scope', function($scope) {
+ 
+}]);
 
-        // Unselect previously selected item.
-        $menuItems.filter('[aria-selected="true"]').attr('aria-selected', false);
+// function routerHandler() {
 
-        // Mark proper menu item as selected.
-        $menuItems.find('[href="' + hash + '"]')
-                  .parent()
-                  .attr('aria-selected', true);
-    }
+//     var $panels = $('[role="panel"]');
+//     var $menuItems = $('[role="navigation"] li');
 
-    window.onpopstate = handleRouteChange;
-    handleRouteChange();
-}
+//     function handleRouteChange() {
 
-window.onload = function() {
-    routerHandler();
-	createNoteHandler();
-	listNoteHandler();
-};
+//         var hash;
+//         // Default screen management.
+//         if(window.location.hash.length === 0) {
+//             hash = "#crud-create";
+//         } else {
+//             hash = window.location.hash;
+//         }
+
+//         // Reset panels visibility.
+//         $panels.hide();
+
+//         // Select panel, and show it.
+//         var $panel = $panels.filter(hash).show();
+
+//         // Unselect previously selected item.
+//         $menuItems.filter('[aria-selected="true"]').attr('aria-selected', false);
+
+//         // Mark proper menu item as selected.
+//         $menuItems.find('[href="' + hash + '"]')
+//                   .parent()
+//                   .attr('aria-selected', true);
+//     }
+
+//     window.onpopstate = handleRouteChange;
+//     handleRouteChange();
+// }
+
+// window.onload = function() {
+//     routerHandler();
+// 	createNoteHandler();
+// 	listNoteHandler();
+// };
 
 
-function createNoteHandler() {
+// function createNoteHandler() {
 
-    var $payload = $('#crud-create-note .payload pre');
-    var $result = $('#crud-create-note .result');
-    var $resultStatus = $result.find(' p.status span');
-    var $resultBody = $result.find('pre');
-    var $title = $('#create-note-title');
-    var $text = $('#create-note-text');
-    var $button = $('#crud-create-note button');
+//     var $payload = $('#crud-create-note .payload pre');
+//     var $result = $('#crud-create-note .result');
+//     var $resultStatus = $result.find(' p.status span');
+//     var $resultBody = $result.find('pre');
+//     var $title = $('#create-note-title');
+//     var $text = $('#create-note-text');
+//     var $button = $('#crud-create-note button');
 
-    function onFieldChange() {
-        var payload = {};
+//     function onFieldChange() {
+//         var payload = {};
 
-        if ($title.val() && $title.val().length > 0) {
-            payload.title = $title.val();
-        }
+//         if ($title.val() && $title.val().length > 0) {
+//             payload.title = $title.val();
+//         }
 
-        if ($text.val() && $text.val().length > 0) {
-            payload.text = $text.val();
-        }
+//         if ($text.val() && $text.val().length > 0) {
+//             payload.text = $text.val();
+//         }
 
-        $payload.html(JSON.stringify(payload, null, 2));
-    }
+//         $payload.html(JSON.stringify(payload, null, 2));
+//     }
 
-    function onSubmit() {
+//     function onSubmit() {
 
-        var payload = $payload.html();
-        $result.removeClass('error').removeClass('success');
+//         var payload = $payload.html();
+//         $result.removeClass('error').removeClass('success');
 
-        $.ajax({
-            'method': 'POST',
-            'url': 'note',
-            'data': payload,
-            'headers': {
-                'content-type': 'application/json'
-            },
-            'complete': function(xhr, textStatus) {
-                $resultStatus.html(xhr.status);
+//         $.ajax({
+//             'method': 'POST',
+//             'url': 'note',
+//             'data': payload,
+//             'headers': {
+//                 'content-type': 'application/json'
+//             },
+//             'complete': function(xhr, textStatus) {
+//                 $resultStatus.html(xhr.status);
 
-                if (xhr.status !== 201) {
-                    $result.addClass('error');
-                    $resultBody.html(xhr.responseText);
-                } else if (!xhr.responseJSON) {
-                    $result.addClass('error');
-                    $resultBody.html('The created document is expected in ' +
-                                     'the response');
-                } else {
-                    $result.addClass('success');
-                    var formatted = JSON.stringify(xhr.responseJSON, null, 2);
-                    $resultBody.html(formatted);
-                }
-            }
-        });
-    }
+//                 if (xhr.status !== 201) {
+//                     $result.addClass('error');
+//                     $resultBody.html(xhr.responseText);
+//                 } else if (!xhr.responseJSON) {
+//                     $result.addClass('error');
+//                     $resultBody.html('The created document is expected in ' +
+//                                      'the response');
+//                 } else {
+//                     $result.addClass('success');
+//                     var formatted = JSON.stringify(xhr.responseJSON, null, 2);
+//                     $resultBody.html(formatted);
+//                 }
+//             }
+//         });
+//     }
 
-    $title.keyup(onFieldChange);
-    $text.keyup(onFieldChange);
-    $button.click(onSubmit);
-}
+//     $title.keyup(onFieldChange);
+//     $text.keyup(onFieldChange);
+//     $button.click(onSubmit);
+// }
 
-function listNoteHandler() {
+// function listNoteHandler() {
 
-    var $result = $('#crud-list-note .result');
-    var $resultStatus = $result.find(' p.status span');
-    var $resultBody = $result.find('pre');
-    var $button = $('#crud-list-note button');
+//     var $result = $('#crud-list-note .result');
+//     var $resultStatus = $result.find(' p.status span');
+//     var $resultBody = $result.find('pre');
+//     var $button = $('#crud-list-note button');
 
-    function onSubmit() {
+//     function onSubmit() {
 
-        $result.removeClass('error').removeClass('success');
+//         $result.removeClass('error').removeClass('success');
 
-        $.ajax({
-            'method': 'GET',
-            'url': 'notes',
-            'complete': function(xhr, textStatus) {
-                $resultStatus.html(xhr.status);
+//         $.ajax({
+//             'method': 'GET',
+//             'url': 'notes',
+//             'complete': function(xhr, textStatus) {
+//                 $resultStatus.html(xhr.status);
 
-                if (xhr.status !== 200) {
-                    $result.addClass('error');
-                    $resultBody.html(xhr.responseText);
-                } else {
-                    $result.addClass('success');
-                    var formatted = JSON.stringify(xhr.responseJSON, null, 2);
-                    $resultBody.html(formatted);
-                }
-            }
-        });
-    }
+//                 if (xhr.status !== 200) {
+//                     $result.addClass('error');
+//                     $resultBody.html(xhr.responseText);
+//                 } else {
+//                     $result.addClass('success');
+//                     var formatted = JSON.stringify(xhr.responseJSON, null, 2);
+//                     $resultBody.html(formatted);
+//                 }
+//             }
+//         });
+//     }
 
-    $button.click(onSubmit);
-}
+//     $button.click(onSubmit);
+// }
